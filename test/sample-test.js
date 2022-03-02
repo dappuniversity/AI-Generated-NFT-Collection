0d33161cdbb1a6e8ce47fc857bfa7890c780a2f1
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2022-03-02 13:58:20 +0100

    rabote testot - se minta edno nft

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,6 +1,10 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
 // describe("Greeter", function () {
 //   it("Should return the new greeting once it's changed", async function () {
 //     const Greeter = await ethers.getContractFactory("Greeter");
@@ -25,15 +29,11 @@ describe("AINFTCollection", function () {
     const aiNFTCollection = await AINFTCollection.deploy();
     await aiNFTCollection.deployed();
 
-    // expect(await greeter.greet()).to.equal("Hello, world!");
-
-    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
     console.log("AI NFT collection is deployed to:", aiNFTCollection.address);
+    const [owner, user1, user2] = await ethers.getSigners();
+    await aiNFTCollection.connect(user2).mint("https://ipfs.io/ipfs/QmeEbzNjpKEbEiN9vCbkTdsN5uWGGA6w5rRF3qPZaj2otE")
 
-    // // wait until the transaction is mined
-    // await setGreetingTx.wait();
-
-    // expect(await greeter.greet()).to.equal("Hola, mundo!");
+    result = await aiNFTCollection.ownerOf(1)
+    result.should.equal(user2.address)
   });
 });
