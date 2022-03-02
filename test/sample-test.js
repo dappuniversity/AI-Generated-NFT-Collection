58859fb958f664fe6895bb1c5988b4ba9e868c79
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2022-03-02 14:01:42 +0100

    refactor the test

--- test/sample-test.js
+++ test/sample-test.js
@@ -5,35 +5,24 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-// describe("Greeter", function () {
-//   it("Should return the new greeting once it's changed", async function () {
-//     const Greeter = await ethers.getContractFactory("Greeter");
-//     const greeter = await Greeter.deploy("Hello, world!");
-//     await greeter.deployed();
-
-//     expect(await greeter.greet()).to.equal("Hello, world!");
-
-//     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-//     // wait until the transaction is mined
-//     await setGreetingTx.wait();
-
-//     expect(await greeter.greet()).to.equal("Hola, mundo!");
-//   });
-// });
-
-
 describe("AINFTCollection", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const AINFTCollection = await ethers.getContractFactory("AINFTCollection");
-    const aiNFTCollection = await AINFTCollection.deploy();
-    await aiNFTCollection.deployed();
 
-    console.log("AI NFT collection is deployed to:", aiNFTCollection.address);
-    const [owner, user1, user2] = await ethers.getSigners();
-    await aiNFTCollection.connect(user2).mint("https://ipfs.io/ipfs/QmeEbzNjpKEbEiN9vCbkTdsN5uWGGA6w5rRF3qPZaj2otE")
+  let aiNFTCollection
+
+  beforeEach(async () => {
+    const AINFTCollection = await ethers.getContractFactory("AINFTCollection");
+    aiNFTCollection = await AINFTCollection.deploy();
+    await aiNFTCollection.deployed();
+    
+    console.log("AI NFT collection was deployed to:", aiNFTCollection.address);
+  }) 
+
+  it("Should return the new greeting once it's changed", async function () {
+    
+    const [user1] = await ethers.getSigners();
+    await aiNFTCollection.connect(user1).mint("https://ipfs.io/ipfs/QmeEbzNjpKEbEiN9vCbkTdsN5uWGGA6w5rRF3qPZaj2otE")
 
     result = await aiNFTCollection.ownerOf(1)
-    result.should.equal(user2.address)
+    result.should.equal(user1.address)
   });
 });
